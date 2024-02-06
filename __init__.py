145d9418ec20411010494952ec436d648d2bceca
Rikki Guy <rikki.guy@opendataservices.coop>
2024-02-06 16:32:07 +0000

    fix(plugin): Add logging setup

--- __init__.py
+++ __init__.py
@@ -1,4 +1,22 @@
+import logging
 from .plugin import OFDSDedupPlugin
 
+
 def classFactory(iface):
+
+    # Setup logging
+    # TODO: change log level by env var
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
     return OFDSDedupPlugin(iface)
