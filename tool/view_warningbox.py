f3ad63cd7d57ea048bf975e989c0445a61145db5
Rikki Guy <rikki.guy@opendataservices.coop>
2024-04-12 16:49:15 +0100

    fix(view): Make the MessageBox text a bit nicer

--- tool/view_warningbox.py
+++ tool/view_warningbox.py
@@ -23,9 +23,10 @@ def show_node_consolidation_warning(
     msg.setWindowTitle(f"Node {a_or_b} {node.name} already consolidated")
     msg.setInformativeText(
         (
-            f"The node {a_or_b} {node.name} (id = {node.id}"
-            + " has already been marked as the same as another node"
-            + f" {other_node.name} (id = {other_node.id})).\n"
+            f"The node in Network {a_or_b}:\n\n"
+            + f"{node.name} (id = {node.id})\n\n"
+            + "has already been marked as the same as another node:\n\n"
+            + f"{other_node.name} (id = {other_node.id})).\n\n"
             + " If you mark  this node as the same instead, this will"
             + " override your previous match.\n"
             + "Are you sure?"
