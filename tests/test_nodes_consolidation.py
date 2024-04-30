681afe7e0749f748cde0394b8ba48ba778f0dd22
Rikki Guy <rikki.guy@opendataservices.coop>
2024-04-30 19:43:07 +0100

    fix(tests): Fix test data path

--- tests/test_nodes_consolidation.py
+++ tests/test_nodes_consolidation.py
@@ -1,4 +1,5 @@
-from qgis.core import QgsVectorLayer
+from pathlib import Path
+from qgis.core import QgsVectorLayer, QgsApplication
 
 from tool.model.comparison import (
     ConsolidationReason,
@@ -7,18 +8,35 @@ from tool.model.comparison import (
 from tool.model.consolidation import NetworkNodesConsolidator
 from tool.model.network import Network
 
-import pytest
+# QgsApplication.setPrefixPath("/usr")  # for Linux
 
 
-@pytest.mark.qgis_new_project()
-def test_nodes_consolidation():
-    a_nodes_layer = QgsVectorLayer("test_data/nodes_a.geojson", "a_nodes")
-    a_spans_layer = QgsVectorLayer("test_data/spans_a.geojson", "a_spans")
-    b_nodes_layer = QgsVectorLayer("test_data/nodes_b.geojson", "b_nodes")
-    b_spans_layer = QgsVectorLayer("test_data/spans_b.geojson", "b_spans")
+def test_nodes_consolidation(qgis_app, qgis_new_project, request):
+
+    test_data_dir = Path(Path(request.path).parent, "test_data").absolute()
+
+    a_nodes_uri = "GeoJSON:" + Path(test_data_dir, "nodes_a.geojson").as_posix()
+    a_spans_uri = "GeoJSON:" + Path(test_data_dir, "spans_a.geojson").as_posix()
+    b_nodes_uri = "GeoJSON:" + Path(test_data_dir, "nodes_b.geojson").as_posix()
+    b_spans_uri = "GeoJSON:" + Path(test_data_dir, "spans_b.geojson").as_posix()
+
+    print(f"{a_nodes_uri=}")
+
+    a_nodes_layer = QgsVectorLayer(a_nodes_uri, "a_nodes", "ogr")
+    a_spans_layer = QgsVectorLayer(a_spans_uri, "a_spans", "ogr")
+    b_nodes_layer = QgsVectorLayer(b_nodes_uri, "b_nodes", "ogr")
+    b_spans_layer = QgsVectorLayer(b_spans_uri, "b_spans", "ogr")
+
+    # Numbers based on the test data layers
+    assert len(list(a_nodes_layer.getFeatures())) == 4
+    assert len(list(b_nodes_layer.getFeatures())) == 4
 
     network_a = Network.from_qgs_vectorlayers(a_nodes_layer, a_spans_layer)
     network_b = Network.from_qgs_vectorlayers(b_nodes_layer, b_spans_layer)
+
+    # Numbers based on the test data layers
+    assert len(network_a.nodes) == 4
+    assert len(network_b.nodes) == 4
 
     nnc = NetworkNodesConsolidator(
         network_a, network_b, merge_above=100, ask_above=0, match_radius_km=10
@@ -52,7 +70,5 @@ def test_nodes_consolidation():
     )
 
     # Numbers based on the test data layers
-    assert len(network_a.nodes) == 4
-    assert len(network_b.nodes) == 4
     assert len(consolidated_net_a.nodes) == 5
     assert len(consolidated_net_b.nodes) == 5
