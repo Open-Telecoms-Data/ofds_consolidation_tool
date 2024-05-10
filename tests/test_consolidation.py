7f22eec2c3427e4b909d3fc203059b7b24dd4472
Rikki Guy <rikki.guy@opendataservices.coop>
2024-05-10 13:29:45 +0100

    fix(tests): Add test for Spans consolidation

--- tests/test_consolidation.py
+++ tests/test_consolidation.py
@@ -1,12 +1,14 @@
 import logging
 from pathlib import Path
+from typing import Tuple
 from qgis.core import QgsVectorLayer
 
 from tool.model.comparison import (
     ConsolidationReason,
     NodeComparisonOutcome,
+    SpanComparisonOutcome,
 )
-from tool.model.consolidation import NetworkNodesConsolidator
+from tool.model.consolidation import NetworkNodesConsolidator, NetworkSpansConsolidator
 from tool.model.network import Network
 
 # QgsApplication.setPrefixPath("/usr")  # for Linux?
@@ -18,8 +20,7 @@ logger = logging.getLogger(__name__)
 setup_logging()
 
 
-def test_nodes_consolidation(qgis_app, qgis_new_project, request):
-
+def load_test_networks(request) -> Tuple[Network, Network]:
     test_data_dir = Path(Path(request.path).parent, "test_data").absolute()
 
     a_nodes_uri = "GeoJSON:" + Path(test_data_dir, "nodes_a.geojson").as_posix()
@@ -37,6 +38,8 @@ def test_nodes_consolidation(qgis_app, qgis_new_project, request):
     # Numbers based on the test data layers
     assert len(list(a_nodes_layer.getFeatures())) == 4
     assert len(list(b_nodes_layer.getFeatures())) == 4
+    assert len(list(a_spans_layer.getFeatures())) == 3
+    assert len(list(b_spans_layer.getFeatures())) == 3
 
     network_a = Network.from_qgs_vectorlayers(a_nodes_layer, a_spans_layer)
     network_b = Network.from_qgs_vectorlayers(b_nodes_layer, b_spans_layer)
@@ -44,17 +47,26 @@ def test_nodes_consolidation(qgis_app, qgis_new_project, request):
     # Numbers based on the test data layers
     assert len(network_a.nodes) == 4
     assert len(network_b.nodes) == 4
+    assert len(network_a.spans) == 3
+    assert len(network_b.spans) == 3
 
+    return (network_a, network_b)
+
+
+def test_consolidation(qgis_app, qgis_new_project, request):
+    network_a, network_b = load_test_networks(request)
+
+    # Test Nodes Consolidation
     nnc = NetworkNodesConsolidator(
         network_a, network_b, merge_above=100, ask_above=0, match_radius_km=10
     )
 
-    comparisons = nnc.get_comparisons_to_ask_user()
+    node_comparisons = nnc.get_comparisons_to_ask_user()
 
-    logger.info(f"{len(comparisons)=}")
+    logger.info(f"{len(node_comparisons)=}")
 
     # Mock the user pressing Same on all comparisons
-    comparison_outcomes = [
+    node_comparison_outcomes = [
         NodeComparisonOutcome(
             comparison=comparison,
             consolidate=ConsolidationReason(
@@ -66,15 +78,50 @@ def test_nodes_consolidation(qgis_app, qgis_new_project, request):
                 manual=True,
             ),
         )
-        for comparison in comparisons
+        for comparison in node_comparisons
     ]
 
-    nnc.add_comparison_outcomes(comparison_outcomes)
+    nnc.add_comparison_outcomes(node_comparison_outcomes)
 
-    (consolidated_net_a, consolidated_net_b) = (
+    (network_a_consolidated_nodes, network_b_consolidated_nodes) = (
         nnc.get_networks_with_consolidated_nodes()
     )
 
     # Numbers based on the test data layers
-    assert len(consolidated_net_a.nodes) == 5
-    assert len(consolidated_net_b.nodes) == 5
+    assert len(network_a_consolidated_nodes.nodes) == 5
+    assert len(network_b_consolidated_nodes.nodes) == 5
+
+    # Test Spans Consolidation
+    nsc = NetworkSpansConsolidator(
+        network_a_consolidated_nodes, network_b_consolidated_nodes
+    )
+
+    span_comparisons = nsc.get_comparisons_to_ask_user()
+
+    # Simulate pressing Same on all comparisons
+    span_comparison_outcomes = [
+        SpanComparisonOutcome(
+            sc,
+            ConsolidationReason(
+                feature_type="SPAN",
+                primary=sc.feature_a,
+                secondary=sc.feature_b,
+                confidence=sc.confidence,
+                matching_properties=sc.get_high_scoring_properties(),
+                manual=True,
+            ),
+        )
+        for sc in span_comparisons
+    ]
+
+    consolidated_network = nsc.get_consolidated_network_with_user_comparison_outcomes(
+        span_comparison_outcomes
+    )
+
+    # Numbers based on test data layers
+    # Check that nodes haven't changed
+    assert len(consolidated_network.nodes) == 5
+    assert len(list(consolidated_network.nodesLayer.getFeatures())) == 5
+    # Check for the expected number of Spans
+    assert len(consolidated_network.spans) == 4
+    assert len(list(consolidated_network.spansLayer.getFeatures())) == 4
